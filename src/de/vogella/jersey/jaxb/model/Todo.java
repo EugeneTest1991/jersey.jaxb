46de32a461a3675665a2de523f9a3739c6109a17
Eugene G <haiduchonak@gmail.com>
2014-08-12 23:46:32 +0300

    add @XmlRootElement description

--- src/de/vogella/jersey/jaxb/model/Todo.java
+++ src/de/vogella/jersey/jaxb/model/Todo.java
@@ -2,6 +2,11 @@ package de.vogella.jersey.jaxb.model;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
+/*
+ * Annotate  a  Java  bean  with  @XmlRootElement,  
+ * and  the  JAXB  framework  (a  part  of  Java  EE)  
+ * will  turn  it  into  an  XML  or  JSON  document. 
+ */
 @XmlRootElement
 public class Todo {
 	private String summary;
